2d2fbbe30d985fffc0a40b6c6e5ed80d4f53f251
Carolina Klein <carolina.e.klein@gmail.com>
2020-04-20 15:11:11 -0400

    intro page initial styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
-import Mission_Booked from './src/assets/missionBooked.jpeg'
+// import Mission_Booked from './src/assets/missionBooked.jpeg'
 
 const Projects = () => {
     return(
         <Card style={{ width: '25rem' }}>
-        <Card.Body>
-        <Card.Img src={Mission_Booked}></Card.Img>
-          <Card.Link href="#"/>
-        </Card.Body>
+            <Card.Body>
+                {/* <Card.Img src={Mission_Booked}></Card.Img> */}
+                <Card.Link href="#"/>
+            </Card.Body>
         </Card>
     )
 }
