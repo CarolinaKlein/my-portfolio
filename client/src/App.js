ec8bfd7a2010a9760551107526ce5e13f20f02fa
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-27 11:34:39 -0400

    yarn added node-sass

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import About from '../src/pages/About/About.jsx'
 import Projects from '../src/pages/Projects/Projects.jsx'
 import Resume from '../src/pages/Resume/Resume.jsx'
 import Contact from '../src/pages/Contact/Contact.jsx'
-import '../src/styles/App.css';
+import '../src/styles/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
