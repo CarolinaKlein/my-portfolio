9b518dae9541f6d341f730120badcca54a2b45c4
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-27 09:18:55 -0400

    Changed homepage tittle

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     return(
         <div className="intro">
             <p>Hello, World! I'm <span>Carolina Klein</span>.</p>
-            <p>I am a Full Stack Web Developer</p>
+            <p>I am a Software Engineer</p>
             <input className="landing-btn" type="button" value="View my work"/>
         </div>
     )
