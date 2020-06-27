fa705099b9efb9941f53744a5887706c90c22961
Carolina Klein <carolina.e.klein@gmail.com>
2020-06-27 11:30:41 -0400

    removed router from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,19 @@ import About from '../src/pages/About/About.jsx'
 import Projects from '../src/pages/Projects/Projects.jsx'
 import Resume from '../src/pages/Resume/Resume.jsx'
 import Contact from '../src/pages/Contact/Contact.jsx'
-import {BrowserRouter as Router} from "react-router-dom";
 import '../src/styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const App = () => (
-  <>
+  <div className="container">
     <Home/>
-    <Router>
-        <Navbar/>
-    </Router>
+    <Navbar/>
     <About/>
     <Projects/>
     <Resume/>
     <Contact/>
-  </>
+  </div>
 );
 
 
