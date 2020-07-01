ccc9b94be1c0ee04491603f96ac47a77f88ba33c
Carolina Klein <carolina.e.klein@gmail.com>
2020-07-01 11:07:58 -0400

    minimal styling changes using bulma

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import './Projects.scss'
-import data from '../../data/projects.json'
+import React from "react";
+import "./Projects.scss";
+import data from "../../data/projects.json";
+import Furever from "../../assets/images/Furever.jpeg"
+import Mission_Booked from "../../assets/images/Mission_Booked.jpeg"
 
 
-function Projects (){
-    
-    return (
-        <section id="projects">
-            <h1>PROJECTS</h1>
-            <div className="colums">
-            {data.projects.map(post => {
-                return(
-                    <div className="projects column" key={post}>
-                        <h2>{post.title}</h2>
-                        <img src={post.img}/>
-                        <a href={post.site}>Live Site</a>
-                        <a href={post.repo}>Source Code</a>
-                    </div>
-                )
-            })}
+function Projects() {
+  return (
+    <section id="projects">
+      <h1>PROJECTS</h1>
+      <div className="columns">
+        {data.projects.map((post) => {
+          return (
+            <div className="column" key={post}>
+              <h3>{post.title}</h3>
+              <img src={post.img}/> <br/>
+              <a href={post.site}>Live Site</a>
+              <a href={post.repo}>Source Code</a>
             </div>
-        </section>
-    )
-
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
