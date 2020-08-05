00e86e333eba61792f5a686491eba480ee48dd36
Carolina Klein <carolina@Carolina-rMBP.local>
2020-08-05 19:18:31 -0400

    included divs for the starts, and some styling that is still not visible

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,7 +6,11 @@ import './Home.scss'
 const Landing = () => {
 
     return(
+        
         <section id="home" className="home">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
             <p>Hello, World! I'm <span>Carolina Klein</span>.</p>
             <p>I am a Software Engineer</p>
             <AnchorLink href="#projects">View my work</AnchorLink>
